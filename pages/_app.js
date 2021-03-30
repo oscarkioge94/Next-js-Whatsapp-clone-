78b4ff6d4f03e9fed8dbed89de3c03fd35e08feb
unknown <mainaoscar94@gmail.com>
2021-03-30 11:14:53 +0300

    working on the login screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useAuthState } from "react-firebase-hooks";
+import {auth, db} from '../firebase';
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth);
+  if(!user) return <Login /> 
   return <Component {...pageProps} />
 }
 
